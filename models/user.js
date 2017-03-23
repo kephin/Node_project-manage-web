5ae376d9e73c3b57244541c7948c1c088cdd465e
kephin <wunshon76325@gmail.com>
2017-03-23 23:15:29 +0800

    hash and check password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const SALT_FACTOR = 10;
 
 const UserSchema = mongoose.Schema({
   name: {
@@ -24,6 +27,25 @@ const UserSchema = mongoose.Schema({
     default: Date.now(),
   },
 });
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  bcrypt.hash(user.password, SALT_FACTOR)
+    .then(hash => {
+      user.password = hash;
+      next();
+    })
+    .catch(err => next(err));
+});
+
+UserSchema.methods.checkPassword = function (guess, next) {
+  const user = this;
+  bcrypt.compare(guess, user.password, (err, isMatch) => {
+    next(err, isMatch);
+  });
+};
 
 const User = mongoose.model('User', UserSchema);
 
